1915b3f4a8fc5e742636b41562579f8e7d1c5381
Miguel-MX1-Producto <miguel.gonzalez@ggv.mx>
2024-11-14 19:01:29 +0000

    MG: se agrega la extracción, transformacion y escritura de inventarios

--- 000_Hechos.R
+++ 000_Hechos.R
@@ -25,65 +25,76 @@
 }
 
 {
-    #================ Funciones ================
-    #Funcion para obtener el path de usuario
-    fGetUserPath <- function(){
+  
+  #================ Funciones ================
+  #Funcion para obtener el path de usuario
+  fGetUserPath <- function(){
       
-      vUserPath <- getwd()  
-      vUserPathExpr <- gregexpr("/", vUserPath, fixed = TRUE)
-      vUserPathExpr <- unlist(vUserPathExpr)
-      vUser <- substr(vUserPath, 1, vUserPathExpr[3] - 1)
+    vUserPath <- getwd()  
+    vUserPathExpr <- gregexpr("/", vUserPath, fixed = TRUE)
+    vUserPathExpr <- unlist(vUserPathExpr)
+    vUser <- substr(vUserPath, 1, vUserPathExpr[3] - 1)
     
-    }
-    #============== Paths ==============
-    #Path de usuario
-    rUser <- fGetUserPath()
+  }
+  
+  #Funcion derecha
+  fRight <- function(x, n){
     
-    #Path de SharePoint
-    #1 <- Desarrollador
-    #0 <- Despliegue
-    Modo <- 1
+    cText <- paste("0000000000000", x, sep = "") 
+    substr(cText, nchar(cText) - (n + 1), nchar(cText))
     
-    rSharePoint <- ifelse(Modo == 1, file.path("Documents", "Development", "REPORTES"), file.path("GrandVision/MX1-MV Supply Chain - Documentos", "Reportes"))
-    
-    #Path de la carpeta tablas
-    rTablas <- file.path(rUser, rSharePoint, "Forecast_Luxottica", "01_Tablas")
-    
-    #Path de la carpeta reportes
-    rReportes <- file.path(rUser, rSharePoint, "Forecast_Luxottica", "02_Reportes")
-    
-    #Path de Qlik
-    rQlik_PVC <- file.path("B:", "Ventas+Vision", "1_Produccion", "3_Datos_Transformados", "PVC")
-    
-    #Path de Qlik Supply
-    rQlik_Supply <- file.path("B:", "Ventas+Vision", "1_Produccion", "3_Datos_Transformados", "SupplyChain")
-    
-    #================ Constantes ================ 
-    
-    #Año 
-    cAnio <- substring((Sys.time()-3), 1, 4)
-    
-    #Mes
-    cMes <- substring((Sys.time()-3), 6, 7)
-    
-    #Día
-    cDia <- substring((Sys.time()-3), 9, 10)
-    
-    #Primer día del mes, segun la fecha actual
-    cPrimerDiaMes <- seq(as.Date(ceiling_date(Sys.Date(), "month")), length = 2, by = "-1 months")[2] 
-    
-    #Fecha (n) meses de venta
-    cMeses <- 2
-    cFechaMeses <- floor_date((today() - 1) %m+% months(- cMeses), "month")
-    
-    #Fecha (n) semanas atras
-    cSemanas <- 8
-    cFechaSemanas <- (today() -1) %m+% weeks(-cSemanas)
-    
-    #Elementos a mantener en el environment
-    vMantener <- c("vMantener")
-    vMantener <- c(vMantener, ls())
-    
+  }
+  
+  
+  #============== Paths ==============
+  #Path de usuario
+  rUser <- fGetUserPath()
+  
+  #Path de SharePoint
+  #1 <- Desarrollador
+  #0 <- Despliegue
+  Modo <- 1
+  
+  rSharePoint <- ifelse(Modo == 1, file.path("Documents", "Development", "REPORTES"), file.path("GrandVision/MX1-MV Supply Chain - Documentos", "Reportes"))
+  
+  #Path de la carpeta tablas
+  rTablas <- file.path(rUser, rSharePoint, "Forecast_Luxottica", "01_Tablas")
+  
+  #Path de la carpeta reportes
+  rReportes <- file.path(rUser, rSharePoint, "Forecast_Luxottica", "02_Reportes")
+  
+  #Path de Qlik
+  rQlik_PVC <- file.path("B:", "Ventas+Vision", "1_Produccion", "3_Datos_Transformados", "PVC")
+  
+  #Path de Qlik Supply
+  rQlik_Supply <- file.path("B:", "Ventas+Vision", "1_Produccion", "3_Datos_Transformados", "SupplyChain")
+  
+  #================ Constantes ================ 
+  
+  #Año 
+  cAnio <- substring((Sys.time()-3), 1, 4)
+  
+  #Mes
+  cMes <- substring((Sys.time()-3), 6, 7)
+  
+  #Día
+  cDia <- substring((Sys.time()-3), 9, 10)
+  
+  #Primer día del mes, segun la fecha actual
+  cPrimerDiaMes <- seq(as.Date(ceiling_date(Sys.Date(), "month")), length = 2, by = "-1 months")[2] 
+  
+  #Fecha (n) meses de venta
+  cMeses <- 2
+  cFechaMeses <- floor_date((today() - 1) %m+% months(- cMeses), "month")
+  
+  #Fecha (n) semanas atras
+  cSemanas <- 8
+  cFechaSemanas <- (today() -1) %m+% weeks(-cSemanas)
+  
+  #Elementos a mantener en el environment
+  vMantener <- c("vMantener")
+  vMantener <- c(vMantener, ls())
+  
 }
 
 #================ Ejecucion ===================
--- 010_Coloca_Archivos.R
+++ 010_Coloca_Archivos.R
@@ -6,6 +6,16 @@
 
 #================ Imortaciones ===================
 
+#articulo de Catalogo
+tArt_Cat <- read.csv(file.path(rQlik_PVC, "ART_CAT.csv"), header = TRUE, sep = ",") %>% 
+  rename_all(toupper) %>% 
+  rename(SKU = 1) %>% 
+  mutate(EAN = fRight(EAN,13)) %>% 
+  arrange(desc(SKU)) %>%
+  select(SKU, ID_TIPO, ID_LINEA, ID_COLOR, POLARIZADO, GRADUABLE, GENERO, EAN, MODELO, EB_NEB, MARCA, ID_PROVEEDOR, PRECIO_L1, COSTO, ID_GENERICO) %>% 
+  unique()
+
+#================ Ejecucion ===================
 #Inventario
 #Path de Proyecto Luxottica
 rLuxottica <- file.path(rUser, rSharePoint, "Luxottica", "02_Reportes", "SEDICO", cAnio)
@@ -19,8 +29,17 @@ q000_Lista_Archivos <- list.files(file.path(rLuxottica), pattern = "") %>%
   arrange(desc(FECHA_CREACION))
 
 #Leer archivo mas reciente
-q001_Inv_SEDICO <- read_excel(file.path(q000_Lista_Archivos$RUTA[1]))
+q001_Inv_SEDICO <- read_excel(file.path(q000_Lista_Archivos$RUTA[1])) %>% 
+  rename_all(toupper) %>% 
+  mutate(EAN = fRight(UPC, 13)) %>% 
+  left_join(tArt_Cat[,c("EAN", "ID_LINEA", "ID_TIPO", "SKU")], by = "EAN") %>% 
+  rename(EXISTENCIA = "AVAILABLE STOCK") %>% 
+  arrange(ID_LINEA, ID_TIPO, desc(EXISTENCIA)) %>% 
+  data.frame() %>% 
+  select(SKU, ID_LINEA, ID_TIPO, EXISTENCIA)
+  
 
-write.csv(file.path(rTablas, "Inventario.csv"), header = TRUE, sep = ",")
+#Escritura
+write.csv(q001_Inv_SEDICO, file.path(rTablas, "Inventario.csv"), row.names = FALSE)
 
 
